d1037297f67b1b1e5f4ef1e455eeb02d3c81a238
Everton Souza Wience <evertonsouzawience@gmail.com>
2023-12-06 17:39:20 -0300

    atualização do projeto json-server

--- src/app/assets/pages/Caixas/index.js
+++ src/app/assets/pages/Caixas/index.js
@@ -1,51 +1,29 @@
- // Função assíncrona para buscar dados JSON
- async function fetchCases() {
-    try {
-        const response = await fetch('/db/db.json');
-        const data = await response.json();
-        return data.itens_CS.cases;
-    } catch (error) {
-        console.error('Erro ao buscar caixas:', error);
+document.addEventListener("DOMContentLoaded", function () {
+// Faz a solicitação HTTP para obter os dados dos casos
+fetch('http://localhost:3000/cases')
+.then(response => response.json())
+.then(data => {
+    // Função para exibir os dados dos casos
+    function displayCases() {
+        const casesContainer = document.getElementById('cases-container');
+        data.forEach(caseItem => {
+            casesContainer.innerHTML += `
+                <div class="max-w-xs sm:w-1/2 md:w-1/4 lg:w-1/4 xl:w-1/4 p-4">
+                    <div class="bg-gray-500 rounded overflow-hidden shadow-lg">
+                        <img src="${caseItem.imagem}" alt="${caseItem.nome}" class="w-full">
+                        <div class="px-6 py-4">
+                            <div class="font-bold text-white text-xl mb-2">${caseItem.nome}</div>
+                            <p class="text-white text-base font-semibold">Preço: R$${caseItem.price}</p>
+                        </div>
+                    </div>
+                </div>
+            `;
+        });
     }
-}
 
-// Função para criar um elemento de cartão para um item
-function createCard(item) {
-    const card = document.createElement("div");
-    card.classList.add("bg-gray-500", "p-4", "rounded-md", "text-center");
+    // Chama a função para exibir os dados dos casos
+    displayCases();
+})
+.catch(error => console.error('Erro ao obter os dados dos casos:', error));
 
-    const image = document.createElement("img");
-    image.src = item.imagem;
-    image.alt = item.nome;
-    image.classList.add("mx-auto"); // Adiciona margem automática para centralizar horizontalmente
-
-    const title = document.createElement("p");
-    title.classList.add("text-sm", "text-gray-800", "font-semibold", "mt-2");
-    title.textContent = item.nome;
-
-    const price = document.createElement("p");
-    price.classList.add("text-sm", "text-white", "mt-2");
-    price.textContent = item.price ? `Preço: ${item.price}` : "Preço indisponível";
-
-    card.appendChild(image);
-    card.appendChild(title);
-    card.appendChild(price);
-
-    return card;
-}
-
-// Função para adicionar caixas à seção correspondente
-async function addCases() {
-    const casesData = await fetchCases();
-    const casesSection = document.getElementById("casesSection");
-
-    casesData.forEach(box => {
-        const card = createCard(box);
-        casesSection.appendChild(card);
-    });
-}
-
-// Carregar caixas quando a página carregar
-window.onload = async function () {
-    await addCases();
-};
+});
